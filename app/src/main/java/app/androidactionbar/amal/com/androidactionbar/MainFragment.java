ebb9eccbc4adfcbd14e408991899e37496d5b2f9
bteric01 <bteric01@louisville.edu>
2015-10-26 16:02:41 -0400

    Added 2 new java classes
    Added 2 new java classes for MainFragment and TextFragment

---
+++ app/src/main/java/app/androidactionbar/amal/com/androidactionbar/MainFragment.java
@@ -0,0 +1,19 @@
+package app.androidactionbar.amal.com.androidactionbar;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Bradley on 10/26/2015.
+ */
+public class MainFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.main_fragment, container, false);
+
+        return view;
+    }
+}
---
+++ app/src/main/java/app/androidactionbar/amal/com/androidactionbar/TextFragment.java
@@ -0,0 +1,24 @@
+package app.androidactionbar.amal.com.androidactionbar;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by Bradley on 10/26/2015.
+ */
+public class TextFragment extends Fragment {
+    TextView text;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.text_fragment, container, false);
+        text = (TextView) view.findViewById(R.id.txt);
+        String menu = getArguments().getString("Menu");
+        text.setText(menu);
+        return view;
+    }
+}
